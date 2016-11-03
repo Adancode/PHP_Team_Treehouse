fd6b6d621d45592d0fd61d8e946acc3bf86b1847
Adam Camacho <adamcamacho8@gmail.com>
2016-11-03 13:45:27 -0500

    working with var_dump

--- Intro_to_Workspaces_and_PHP_Code/hello.php
+++ Intro_to_Workspaces_and_PHP_Code/hello.php
@@ -4,8 +4,12 @@
           *
           * @author: Adam Camacho
      */
+
      //our first php script
      //echo 'Hello World!'; //done
+
      echo 'Hello Treehouse!';
+
+     # This is a comment
 
  ?>
