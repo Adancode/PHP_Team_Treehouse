3dfc0b66cb9777fe9fe6289b65d8abfeb968bc4c
Adam Camacho <adamcamacho8@gmail.com>
2016-11-04 21:33:35 -0500

    adding exercise.php

---
+++ PHP_Basics/exercise.php
@@ -0,0 +1,3 @@
+<?php
+  // store each exercise in a string variable.
+?>
--- PHP_Basics/strings.php
+++ PHP_Basics/strings.php
@@ -1,9 +1,22 @@
 <?php
 $name = 'Adam';
 $string_one = "One: Learning to display \"Hello $name\" to the screen.\n";  // double quotes allow for displaying variable values, as opposed to the literal variable name.
-$string_two = 'Two: Learning to display \"Hello $name\" to the screen.' . "\n";  // single quotes are literal, only use \' for an apostrophe, that's it.
-echo $string_one;
-echo $string_two;
-$string_three = 'Three: Learning to display "Hello ' . $name . '" to the screen.' . "\n";  // the . in PHP is like the + sign in JavaScript in this case.
-echo $string_three;
+// $string_two = 'Two: Learning to display \"Hello $name\" to the screen.' . "\n";  // single quotes are literal, only use \' for an apostrophe, that's it.
+//echo $string_one;
+// echo $string_two;
+// $string_three = 'Three: Learning to display "Hello ' . $name . '" to the screen.' . "\n";  // the . in PHP is like the + sign in JavaScript in this case.
+// .= is the same as += in JavaScript
+// echo $string_three;
+//
+// $isReady = true;
+// $isReady = false;
+
+// var_dump($isReady);
+// echo $isReady;
+if ($string_one === $string_one + 6) {
+     var_dump("The values are identical");
+} else {
+     echo "the values DO NOT match";
+}
+
  ?>
