123d97123fa3595e50655d54d47452b6f656dc6b
Adam Camacho <adamcamacho8@gmail.com>
2016-11-13 23:54:09 -0600

    working with arrays

--- PHP_Arrays_And_Conditionals/indexed_arrays.php
+++ PHP_Arrays_And_Conditionals/indexed_arrays.php
@@ -1,6 +1,32 @@
 <?php
      $learn = array('Conditionals', 'Arrays', 'Loops');
      $learn[] = 'Build something awesome!';
+     array_push($learn, 'Functions', 'Forms', 'Objects');  // Adds items to the end, will add the next index number in order
+     array_unshift($learn, "First Item");  //  Add items to the beginning, will update the entire index (to make the addition index 0
+     var_dump($learn);
+     $first_element_shifted_out = array_shift($learn);  // removes and returns the first enchant_dict_store_replacement
+     $last_element_popped_out = array_pop($learn); // removes and returns the last element
+     echo $first_element_shifted_out;
+     echo $last_element_popped_out;
+     unset($learn[1], $learn[2]);  // removes the specified values and keys, but the remaining keys keep the same number, instead of being assigned new numbers.
      //var_dump($learn);
-     echo explode("\n", $learn);
+     //var_dump($learn); //explode($learn, "\n");
+     //echo implode( "\n", $learn);
+     $learn = array_values($learn);  // creates new array with sequentioal array keys (useful when we used unset and want the array to have consecutive keys again)
 ?>
+
+<!-- Another option is using array_values() after unset():
+
+$array = array(0, 1, 2, 3);
+
+unset($array[2]);
+$array = array_values($array);
+var_dump($array);
+array(3) {
+  [0]=>
+  int(0)
+  [1]=>
+  int(1)
+  [2]=>
+  int(3)
+}  -->
